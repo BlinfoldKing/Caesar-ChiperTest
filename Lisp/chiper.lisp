e2cde253f71cf736c0693f1a505f4e926842d8b0
Ganesha Danu <ganeshadanu1@gmail.com>
2018-03-26 22:23:45 +0700

    Lisp Version Complete

---
+++ Lisp/chiper.lisp
@@ -0,0 +1,26 @@
+;;  (defun encrypt (str1 key)
+;;      (loop for letter across str1 do 
+;;          (princ letter)))
+;;  (encrypt "hello " 5)
+
+
+(defun shiftChar (chr key)
+    "Encrypting a char based of a following key"
+    (setq k (mod key 25))
+    (if (and (string<= chr "z") (string>= chr "a"))
+        (if (> ( + (char-code chr) k ) (char-code #\z))
+            (progn 
+                (setq k ( - ( + (char-code chr) k) char-code #\z))
+                (setq chr (code-char ( + (char-code #\a) k))))
+            (setq chr (code-char ( + (char-code chr) k)))))
+    chr)
+
+ (defun encrypt (str key)
+    "Traversing each letter and shifting it base on the key value"
+    (defvar res)
+    (setq res "")
+    (loop for letter across str do 
+         (setq res (concatenate 'string res (string (shiftChar letter key)))))
+    res)
+
+(write-line (encrypt "Hello World" 5))
